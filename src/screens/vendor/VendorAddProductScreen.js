0f3394f7b221e83d1b21219ff9d44549a5c9f326
Sravani Polamreddy <sravlaks18112015@gmail.com>
2026-05-30 12:02:23 -0400

    Fix: import useEffect in VendorAddProductScreen

--- src/screens/vendor/VendorAddProductScreen.js
+++ src/screens/vendor/VendorAddProductScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View, Text, TouchableOpacity, StyleSheet,
   ScrollView, KeyboardAvoidingView, Platform, TextInput, ActivityIndicator, Alert, Image, Switch,
